3894458a427e245cde230bfb9c925edde1762908
Eldkaii <54186786+Eldkaii@users.noreply.github.com>
2020-03-21 20:22:42 -0300

    Agregado carpeta heders y algunas pruebas con clases de estilo y titulos

--- PaginaWeb/src/app/app.module.ts
+++ PaginaWeb/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderPrincipalComponent } from './Componentes/Headers/header-principal/header-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPrincipalComponent
   ],
   imports: [
     BrowserModule
